4711e16afcd3148ef391dda4c9900938ad589012
Faisal-Chauhan27 <140692486+Faisal-Chauhan27@users.noreply.github.com>
2024-06-25 19:06:41 +0530

    Add files via upload

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Faisal")
+
+
+// mdn documentation 
+// https://tc39.es/ecma262/#sec-intro
+
+
+
+let name = "Faisal"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof "name")
